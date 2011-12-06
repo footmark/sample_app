5b03ad476208846de839c8d97d7ef4c5ae9bfbc6
Lei Z <service.footmark@gmail.com>
2011-12-06 14:32:38 +0800

    Done with sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Return logo image tag on a per-page basis
   def logo
-    logo = image_tag("logo.png", alt: "Sample App", class: "round")
+    image_tag("logo.png", alt: "Sample App", class: "round")
   end
   
   # Return a title on a per-page basis.
